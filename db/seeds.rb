8f98804cda5800dd64b82af347df6f7ecc1516b9
David Belka <david.belka@yahoo.co.uk>
2019-11-13 13:57:40 +0000

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,10 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating restaurants..."
+
+Restaurant.create(name: 'Hoxton 100', address: 'Hoxton', category: 'chinese')
+Restaurant.create(name: 'Thai Song', address: 'Hoxton1', category: 'japanese')
+Restaurant.create(name: 'Amici Mieu', address: 'Hoxton2', category: 'italian')
+Restaurant.create(name: 'Olive', address: 'Hoxton3', category: 'french')
+Restaurant.create(name: 'Dominos', address: 'Hoxton4', category: 'belgian')
